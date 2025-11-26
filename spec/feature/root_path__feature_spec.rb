2fc527e617dcf353055de15b54478062ad08402b
Devon Kiss <devon@3dmandt.com>
2025-11-26 16:40:22 -0600

    We are coming at it great. With some nice sanity checks and warmup testing to begin the project, all is green.

--- spec/feature/root_path__feature_spec.rb
+++ spec/feature/root_path__feature_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe 'Feature Spec', type: :feature do
     visit root_path
     expect(page.status_code).to eq(200)
   end
+
+  it 'has a title of Dev\'s Notes' do
+    visit root_path
+    page_title = page.find('h1')
+    expect(page_title).to have_content("Dev's Notes")
+  end
 end
